86069b74926db06b405f0b20732316ea92a40303
Alex Mizrahi <alex.mizrahi@gmail.com>
2013-06-07 15:53:30 +0300

    added write-value

--- package.lisp
+++ package.lisp
@@ -3,6 +3,8 @@
 (defpackage #:cl-coin
   (:use #:cl)
   (:import-from :com.gigamonkeys.binary-data 
+                #:read-value
+                #:write-value
                 #:define-binary-type
                 #:define-binary-class))
 
